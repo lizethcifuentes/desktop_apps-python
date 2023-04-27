b9ab325c8e14dc147425c304f2f905da7fb5c75f
juandavid28 <juanrodriguez1@colegioguanenta.edu.co>
2023-04-27 15:29:09 -0500

    se agregan cambios

--- desktop_app-01.py
+++ desktop_app-01.py
@@ -16,5 +16,21 @@ from tkinter import *
 #se declara una variable llamada ventana_principal, que adquiere las caracteristicas de un objeto TK()
 ventana_principal = Tk()
 
+
+# titulo de la ventana
+ventana_principal.title("especialidad en sisitemas guanenta")
+
+# tamaño de la ventana
+ventana_principal.geometry("500x500")
+
+#run 
+ventana_principal.mainloop()
+
+#deshabilitar boton de maximisar
+ventana_principal.resizable(False, False)
+
+#color de fondo de la ventana
+ventana_principal.config(bg="white")
+
 #run
 ventana_principal.mainloop()
